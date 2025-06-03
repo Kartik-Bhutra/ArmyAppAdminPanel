e5e5972c448c411259d32fc3cd465829092f893e
Kartik-Bhutra <kartikbhutra02@gmail.com>
2025-06-04 00:54:38 +0530

    removing and adding encryption key

--- app/clients/approved/page.js
+++ app/clients/approved/page.js
@@ -1,18 +1,6 @@
-import Table from "../(components)/Table";
-import BackButton from "../(components)/BackButton";
-import { db } from "@/lib/firebaseConfig";
-
-export default function ApprovedPage() {
-  
+export default async function ApprovedPage({ searchParams }){
+  const params = await searchParams;
   return (
-    <div className="p-6 max-w-[1400px] mx-auto">
-      <div className="bg-white rounded-xl shadow-lg p-6">
-        <div className="flex justify-between items-center mb-6">
-          <h1 className="text-2xl font-bold text-gray-800">Approved Clients</h1>
-          <BackButton />
-        </div>
-        <Table showActions={false} />
-      </div>
-    </div>
+    <div></div>
   );
 }
--- app/clients/requests/page.js
+++ app/clients/requests/page.js
@@ -1,16 +1,6 @@
-import Table from "../(components)/Table";
-import BackButton from "../(components)/BackButton";
-
-export default function RequestsPage() {
+export default async function RequestPage({ searchParams }) {
+  const params = await searchParams;
   return (
-    <div className="p-6 max-w-[1400px] mx-auto">
-      <div className="bg-white rounded-xl shadow-lg p-6">
-        <div className="flex justify-between items-center mb-6">
-          <h1 className="text-2xl font-bold text-gray-800">Client Requests</h1>
-          <BackButton />
-        </div>
-        <Table showActions={true} />
-      </div>
-    </div>
+    <div></div>
   );
 }
